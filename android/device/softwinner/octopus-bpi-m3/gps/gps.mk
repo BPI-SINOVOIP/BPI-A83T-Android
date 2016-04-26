b0fb67a2d3ecb5e88280ed96f0cb27fc56cfb631
justin <justin@justin-OptiPlex-3010.(none)>
2016-04-26 10:10:49 +0800

    Upload octopus-bpi-m3 module

---
+++ android/device/softwinner/octopus-bpi-m3/gps/gps.mk
@@ -0,0 +1,7 @@
+# gps
+PRODUCT_COPY_FILES += \
+    device/softwinner/octopus-bpi-m3/gps/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
+    device/softwinner/octopus-bpi-m3/gps/gpslogd:system/bin/gpslogd \
+    device/softwinner/octopus-bpi-m3/gps/glgps:system/bin/glgps \
+    device/softwinner/octopus-bpi-m3/gps/gps.conf:system/etc/gps.conf \
+    device/softwinner/octopus-bpi-m3/gps/gps.exdroid.so:system/lib/hw/gps.exdroid.so
